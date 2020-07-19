f878c8b0f37d487a07c6f9f9d32861510337c503
William Austin <wyldebill00@hotmail.com>
2020-07-19 11:50:25 -0500

    Locationpermissionchecks (#2)
    * adding location permission requests and location service on device check
    
    * add permission check on device location and app
    
    Co-authored-by: william austin <william@v3solutions.net>

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,109 @@
 import 'dart:async';
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:location_permissions/location_permissions.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Buffalo Retail Group",
+      home: HomeApp(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class HomeApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _HomeAppState createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  // i have to figure out the Completer(), Future and .complete() relationship soon!
   Completer<GoogleMapController> _controller = Completer();
 
   final Set<Marker> _markers = {};
+
+  // for now, static start location of Buffalo.
   static const LatLng _center = const LatLng(45.1719084, -93.8746941);
   String _mapStyle;
 
-  void _onMapCreated(GoogleMapController controller) {
+  void _onMapCreated(GoogleMapController controller) async {
     controller.setMapStyle(_mapStyle);
     _controller.complete(controller);
+
+    // let's do some checkups here
+    // is the device location service enabled?
+    await _checkDeviceLocationServiceStatus();
+
+    // check for location services permissions for this app.
+    // and ask for them if we
+    // don't have them yet, or
+    // we had them and the user then revoked them from us
+    await LocationPermissions()
+        .requestPermissions(
+            permissionLevel: LocationPermissionLevel.locationWhenInUse)
+        .then((PermissionStatus status) {
+      if (status == PermissionStatus.denied) {
+        print(
+            'DEBUG YOURE THE BOSS BUT THIS APP ISNT MUCH GOOD WITHOUT LOCATION');
+      }
+    });
+  }
+
+  void _showAlertDialog() {
+    // set up the button
+    Widget okButton = FlatButton(
+      child: Text('Ok'),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text('Device Location Service disabled'),
+      content: Text('You need to enable Location Services on this device'),
+      actions: [
+        okButton,
+      ],
+    );
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  void _markerTapped()
+  {
+    
+  }
+  void _checkDeviceLocationServiceStatus() async {
+    ServiceStatus status = await LocationPermissions().checkServiceStatus();
+    if (status == ServiceStatus.disabled) {
+      print("SERVICESTATUS: NO DEVICE LOCATION SERVICES FOUND. POP SETTINGS?");
+      await LocationPermissions().shouldShowRequestPermissionRationale(
+          permissionLevel: LocationPermissionLevel.locationWhenInUse);
+      _showAlertDialog();
+    }
+    if (status == ServiceStatus.enabled) {
+      print(
+          "SERVICESTATUS: WE ARE GOOD TO GO.  DEVICE LOCATION SERVICES ENABLED!!");
+    }
+
+    // not applicable, this device doesn't have location services? it's a TV?
+    // unknown, we cannot get info on device location services at all
   }
 
   @override
@@ -36,8 +117,32 @@ class _MyAppState extends State<MyApp> {
         position: LatLng(45.170712, -93.874553),
         infoWindow: InfoWindow(
             title: 'Hayes Public House', snippet: 'Try the Cliodhana'),
+            onTap: _markerTapped(),
         icon: BitmapDescriptor.defaultMarker));
 
+    _markers.add(Marker(
+        markerId: MarkerId('Biggs and Co.'),
+        position: LatLng(45.172144, -93.874352),
+        infoWindow: InfoWindow(
+            title: 'Biggs and Company',
+            snippet: 'Rustic Industrial and Custom Designs'),
+        icon: BitmapDescriptor.defaultMarker));
+
+    _markers.add(Marker(
+        markerId: MarkerId('A Wreath of Franklin'),
+        position: LatLng(45.172138, -93.875827),
+        infoWindow:
+            InfoWindow(title: 'A Wreath of Franklin', snippet: 'Apparell'),
+        icon: BitmapDescriptor.defaultMarker));
+
+    _markers.add(Marker(
+        markerId: MarkerId('Buffalo Rock Winery'),
+        position: LatLng(45.117736, -93.795735),
+        infoWindow:
+            InfoWindow(title: 'Buffalo Rock Winery', snippet: 'Frappes'),
+        icon: BitmapDescriptor.defaultMarker));
+
+    // i have a custom leaned out map style. no distracting features, minimal.
     rootBundle.loadString('assets/mapstyle/minimal.json').then((string) {
       _mapStyle = string;
     });
@@ -55,6 +160,7 @@ class _MyAppState extends State<MyApp> {
         ),
 
         //what the hell, why doesn't mylocationenabled/button work???
+        // because emulator.  try a real device and it works???
         //http://flutterdevs.com/blog/google-maps-in-flutter/
         body: GoogleMap(
           myLocationButtonEnabled: true,
@@ -63,7 +169,7 @@ class _MyAppState extends State<MyApp> {
           markers: Set<Marker>.of(_markers),
           initialCameraPosition: CameraPosition(
             target: _center,
-            zoom: 18.0,
+            zoom: 10.0,
           ),
         ),
       ),
