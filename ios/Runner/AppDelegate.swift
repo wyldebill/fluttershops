bd0e6cade602f9d73d084b1444314093f145918b
william austin <william@v3solutions.net>
2020-08-01 22:05:25 -0500

    add codemagic secrets for api keys

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,7 +10,7 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    GMSServices.provideAPIKey("xxx")
+    GMSServices.provideAPIKey("$googlemapsapikeyios")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
