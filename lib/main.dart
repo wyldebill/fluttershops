0beab2803dba8f18950d24d83a277e8bbb83abb8
william austin <william@v3solutions.net>
2020-08-15 09:33:42 -0500

    add more markers, clean up descriptions

--- lib/main.dart
+++ lib/main.dart
@@ -150,40 +150,111 @@ class _HomeAppState extends State<HomeApp> {
     // });
 
     _markers.add(Marker(
-        markerId: MarkerId('HayesPublicHouse'),
-        position: LatLng(45.170712, -93.874553),
+        markerId: MarkerId('RitzyReplay'),
+        position: LatLng(45.171706, -93.874609),
         infoWindow: InfoWindow(
-            title: 'Hayes',
-            snippet: 'Try the Cliodhana!',
+            title: 'Riyzy Replay',
+            snippet: 'Clothing',
             onTap: () {
               Navigator.push(_myBuildContext,
                   MaterialPageRoute(builder: (context) => SecondRoute()));
             }),
         onTap: () {
-          print('Hayes was tapped!!!!');
+          print(' tapped on marker !!!!');
         },
         icon: BitmapDescriptor.defaultMarker));
 
     _markers.add(Marker(
         markerId: MarkerId('Biggs and Co.'),
         position: LatLng(45.172144, -93.874352),
-        infoWindow: InfoWindow(
-            title: 'Biggs and Company',
-            snippet: 'Rustic Industrial and Custom Designs'),
+        infoWindow: InfoWindow(title: 'Biggs and Company', snippet: 'Decor'),
         icon: BitmapDescriptor.defaultMarker));
 
     _markers.add(Marker(
         markerId: MarkerId('A Wreath of Franklin'),
-        position: LatLng(45.172138, -93.875827),
+        position: LatLng(45.172253, -93.875635),
         infoWindow:
-            InfoWindow(title: 'A Wreath of Franklin', snippet: 'Apparell'),
+            InfoWindow(title: 'A Wreath of Franklin', snippet: 'Apparel'),
         icon: BitmapDescriptor.defaultMarker));
 
     _markers.add(Marker(
         markerId: MarkerId('Buffalo Rock Winery'),
         position: LatLng(45.117736, -93.795735),
+        infoWindow: InfoWindow(title: 'Buffalo Rock Winery', snippet: 'Wine'),
+        icon: BitmapDescriptor.defaultMarker));
+
+    _markers.add(Marker(
+        markerId: MarkerId('Now and Again'),
+        position: LatLng(45.172950, -93.874602),
+        infoWindow: InfoWindow(title: 'Now and Again', snippet: 'Decor'),
+        icon: BitmapDescriptor.defaultMarker));
+
+    _markers.add(Marker(
+        markerId: MarkerId('Second Hand Rose'),
+        position: LatLng(45.178439, -93.872743),
+        infoWindow: InfoWindow(title: 'Second Hand Rose', snippet: 'Apparel'),
+        icon: BitmapDescriptor.defaultMarker));
+
+    _markers.add(Marker(
+        markerId: MarkerId('JTB Home Furniture and Decor'),
+        position: LatLng(45.172950, -93.875904),
         infoWindow:
-            InfoWindow(title: 'Buffalo Rock Winery', snippet: 'Frappes'),
+            InfoWindow(title: 'JTB Home Furniture and Decor', snippet: 'Decor'),
+        icon: BitmapDescriptor.defaultMarker));
+
+    _markers.add(Marker(
+        markerId: MarkerId('She'),
+        position: LatLng(45.172177, -93.876115),
+        infoWindow: InfoWindow(title: 'SHE', snippet: 'Decor'),
+        icon: BitmapDescriptor.defaultMarker));
+
+    _markers.add(Marker(
+        markerId: MarkerId('The Porch in Buffalo'),
+        position: LatLng(45.173125, -93.875940),
+        infoWindow: InfoWindow(title: 'The Porch in Buffalo', snippet: 'Decor'),
+        icon: BitmapDescriptor.defaultMarker));
+
+    _markers.add(Marker(
+        markerId: MarkerId('The Rustic Arbor'),
+        position: LatLng(45.172677, -93.876039),
+        infoWindow: InfoWindow(title: 'The Rustic Arbor', snippet: 'Decor'),
+        icon: BitmapDescriptor.defaultMarker));
+
+    _markers.add(Marker(
+        markerId: MarkerId('What' 's the Scoop'),
+        position: LatLng(45.172143, -93.875460),
+        infoWindow: InfoWindow(title: 'What' 's the Scoop', snippet: 'Food'),
+        icon: BitmapDescriptor.defaultMarker));
+
+    _markers.add(Marker(
+        markerId: MarkerId('Harlows House'),
+        position: LatLng(45.172802, -93.874704),
+        infoWindow: InfoWindow(title: 'Harlows House', snippet: 'Decor'),
+        icon: BitmapDescriptor.defaultMarker));
+
+    _markers.add(Marker(
+        markerId: MarkerId('Lillians'),
+        position: LatLng(45.172683, -93.875803),
+        infoWindow: InfoWindow(title: 'Lillians', snippet: 'Clothing'),
+        icon: BitmapDescriptor.defaultMarker));
+
+    _markers.add(Marker(
+        markerId: MarkerId('Evelyn' 's'),
+        position: LatLng(45.172666, -93.876034),
+        infoWindow: InfoWindow(title: 'Evelyn' 's', snippet: 'Wine'),
+        icon: BitmapDescriptor.defaultMarker));
+
+    _markers.add(Marker(
+        markerId: MarkerId('Behind the Picket Fence'),
+        position: LatLng(45.172865, -93.876104),
+        infoWindow:
+            InfoWindow(title: 'Behind the Picket Fence', snippet: 'Decor'),
+        icon: BitmapDescriptor.defaultMarker));
+
+    _markers.add(Marker(
+        markerId: MarkerId('This and That'),
+        position: LatLng(45.178114, -93.873020),
+        infoWindow: InfoWindow(title: 'This and That', snippet: 'Decor'),
         icon: BitmapDescriptor.defaultMarker));
 
     // i have a custom leaned out map style. no distracting features, minimal.
