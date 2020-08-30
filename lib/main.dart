8dc8c5b47c09bfec9ec213bc7818ff3ae4b9e381
william austin <william@v3solutions.net>
2020-08-30 15:13:06 -0500

    remove map markers for stores that aren't open. add fab. add event detail to all markers. move detail page to separate file

--- lib/main.dart
+++ lib/main.dart
@@ -5,40 +5,22 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:location_permissions/location_permissions.dart';
+import 'package:mapstesting/secondroute.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-// blank mostly widget to test navigation and the back button
-class SecondRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Store Detail here"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
-      ),
-    );
-  }
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Buffalo",
+      title: "Buffalo", //?? what is this even for?
       home: DefaultTabController(
         length: 2,
         child: Scaffold(
           appBar: AppBar(
+            title: new Text('Buffalo Retail Group'),
             bottom: TabBar(
               tabs: [
                 Tab(icon: Icon(Icons.map)),
@@ -49,8 +31,9 @@ class MyApp extends StatelessWidget {
           body: TabBarView(
             physics: NeverScrollableScrollPhysics(),
             children: [
-              HomeApp(),
-              Icon(Icons.directions_transit),
+              // each tab needs an entry here
+              HomeApp(), // map
+              Icon(Icons.library_books), // listview
             ],
           ),
         ),
@@ -153,7 +136,7 @@ class _HomeAppState extends State<HomeApp> {
         markerId: MarkerId('RitzyReplay'),
         position: LatLng(45.171706, -93.874609),
         infoWindow: InfoWindow(
-            title: 'Riyzy Replay',
+            title: 'Ritzy Replay',
             snippet: 'Clothing',
             onTap: () {
               Navigator.push(_myBuildContext,
@@ -167,94 +150,182 @@ class _HomeAppState extends State<HomeApp> {
     _markers.add(Marker(
         markerId: MarkerId('Biggs and Co.'),
         position: LatLng(45.172144, -93.874352),
-        infoWindow: InfoWindow(title: 'Biggs and Company', snippet: 'Decor'),
-        icon: BitmapDescriptor.defaultMarker));
+        infoWindow: InfoWindow(
+            title: 'Biggs and Company',
+            snippet: 'Decor',
+            onTap: () {
+              Navigator.push(_myBuildContext,
+                  MaterialPageRoute(builder: (context) => SecondRoute()));
+            }),
+        icon:
+            BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen)));
 
     _markers.add(Marker(
         markerId: MarkerId('A Wreath of Franklin'),
         position: LatLng(45.172253, -93.875635),
-        infoWindow:
-            InfoWindow(title: 'A Wreath of Franklin', snippet: 'Apparel'),
+        infoWindow: InfoWindow(
+            title: 'A Wreath of Franklin',
+            snippet: 'Apparel',
+            onTap: () {
+              Navigator.push(_myBuildContext,
+                  MaterialPageRoute(builder: (context) => SecondRoute()));
+            }),
         icon: BitmapDescriptor.defaultMarker));
 
     _markers.add(Marker(
         markerId: MarkerId('Buffalo Rock Winery'),
         position: LatLng(45.117736, -93.795735),
-        infoWindow: InfoWindow(title: 'Buffalo Rock Winery', snippet: 'Wine'),
+        infoWindow: InfoWindow(
+            title: 'Buffalo Rock Winery',
+            snippet: 'Wine',
+            onTap: () {
+              Navigator.push(_myBuildContext,
+                  MaterialPageRoute(builder: (context) => SecondRoute()));
+            }),
         icon: BitmapDescriptor.defaultMarker));
 
     _markers.add(Marker(
         markerId: MarkerId('Now and Again'),
         position: LatLng(45.172950, -93.874602),
-        infoWindow: InfoWindow(title: 'Now and Again', snippet: 'Decor'),
+        infoWindow: InfoWindow(
+            title: 'Now and Again',
+            snippet: 'Decor',
+            onTap: () {
+              Navigator.push(_myBuildContext,
+                  MaterialPageRoute(builder: (context) => SecondRoute()));
+            }),
         icon: BitmapDescriptor.defaultMarker));
 
     _markers.add(Marker(
         markerId: MarkerId('Second Hand Rose'),
         position: LatLng(45.178439, -93.872743),
-        infoWindow: InfoWindow(title: 'Second Hand Rose', snippet: 'Apparel'),
+        infoWindow: InfoWindow(
+            title: 'Second Hand Rose',
+            snippet: 'Apparel',
+            onTap: () {
+              Navigator.push(_myBuildContext,
+                  MaterialPageRoute(builder: (context) => SecondRoute()));
+            }),
         icon: BitmapDescriptor.defaultMarker));
 
     _markers.add(Marker(
         markerId: MarkerId('JTB Home Furniture and Decor'),
         position: LatLng(45.172950, -93.875904),
-        infoWindow:
-            InfoWindow(title: 'JTB Home Furniture and Decor', snippet: 'Decor'),
+        infoWindow: InfoWindow(
+            title: 'JTB Home Furniture and Decor',
+            snippet: 'Decor',
+            onTap: () {
+              Navigator.push(_myBuildContext,
+                  MaterialPageRoute(builder: (context) => SecondRoute()));
+            }),
         icon: BitmapDescriptor.defaultMarker));
 
     _markers.add(Marker(
-        markerId: MarkerId('She'),
+        markerId: MarkerId('SHE'),
         position: LatLng(45.172177, -93.876115),
-        infoWindow: InfoWindow(title: 'SHE', snippet: 'Decor'),
+        infoWindow: InfoWindow(
+            title: 'SHE',
+            snippet: 'Decor',
+            onTap: () {
+              Navigator.push(_myBuildContext,
+                  MaterialPageRoute(builder: (context) => SecondRoute()));
+            }),
         icon: BitmapDescriptor.defaultMarker));
 
     _markers.add(Marker(
         markerId: MarkerId('The Porch in Buffalo'),
         position: LatLng(45.173125, -93.875940),
-        infoWindow: InfoWindow(title: 'The Porch in Buffalo', snippet: 'Decor'),
+        infoWindow: InfoWindow(
+            title: 'The Porch in Buffalo',
+            snippet: 'Decor',
+            onTap: () {
+              Navigator.push(_myBuildContext,
+                  MaterialPageRoute(builder: (context) => SecondRoute()));
+            }),
         icon: BitmapDescriptor.defaultMarker));
 
     _markers.add(Marker(
         markerId: MarkerId('The Rustic Arbor'),
         position: LatLng(45.172677, -93.876039),
-        infoWindow: InfoWindow(title: 'The Rustic Arbor', snippet: 'Decor'),
+        infoWindow: InfoWindow(
+            title: 'The Rustic Arbor',
+            snippet: 'Decor',
+            onTap: () {
+              Navigator.push(_myBuildContext,
+                  MaterialPageRoute(builder: (context) => SecondRoute()));
+            }),
         icon: BitmapDescriptor.defaultMarker));
 
     _markers.add(Marker(
         markerId: MarkerId('What' 's the Scoop'),
         position: LatLng(45.172143, -93.875460),
-        infoWindow: InfoWindow(title: 'What' 's the Scoop', snippet: 'Food'),
+        infoWindow: InfoWindow(
+            title: 'What' 's the Scoop',
+            snippet: 'Food',
+            onTap: () {
+              Navigator.push(_myBuildContext,
+                  MaterialPageRoute(builder: (context) => SecondRoute()));
+            }),
         icon: BitmapDescriptor.defaultMarker));
 
     _markers.add(Marker(
         markerId: MarkerId('Harlows House'),
         position: LatLng(45.172802, -93.874704),
-        infoWindow: InfoWindow(title: 'Harlows House', snippet: 'Decor'),
+        infoWindow: InfoWindow(
+            title: 'Harlows House',
+            snippet: 'Decor',
+            onTap: () {
+              Navigator.push(_myBuildContext,
+                  MaterialPageRoute(builder: (context) => SecondRoute()));
+            }),
         icon: BitmapDescriptor.defaultMarker));
 
     _markers.add(Marker(
         markerId: MarkerId('Lillians'),
         position: LatLng(45.172683, -93.875803),
-        infoWindow: InfoWindow(title: 'Lillians', snippet: 'Clothing'),
+        infoWindow: InfoWindow(
+            title: 'Lillians',
+            snippet: 'Clothing',
+            onTap: () {
+              Navigator.push(_myBuildContext,
+                  MaterialPageRoute(builder: (context) => SecondRoute()));
+            }),
         icon: BitmapDescriptor.defaultMarker));
 
     _markers.add(Marker(
         markerId: MarkerId('Evelyn' 's'),
         position: LatLng(45.172666, -93.876034),
-        infoWindow: InfoWindow(title: 'Evelyn' 's', snippet: 'Wine'),
+        infoWindow: InfoWindow(
+            title: 'Evelyn' 's',
+            snippet: 'Wine',
+            onTap: () {
+              Navigator.push(_myBuildContext,
+                  MaterialPageRoute(builder: (context) => SecondRoute()));
+            }),
         icon: BitmapDescriptor.defaultMarker));
 
     _markers.add(Marker(
         markerId: MarkerId('Behind the Picket Fence'),
         position: LatLng(45.172865, -93.876104),
-        infoWindow:
-            InfoWindow(title: 'Behind the Picket Fence', snippet: 'Decor'),
+        infoWindow: InfoWindow(
+            title: 'Behind the Picket Fence',
+            snippet: 'Decor',
+            onTap: () {
+              Navigator.push(_myBuildContext,
+                  MaterialPageRoute(builder: (context) => SecondRoute()));
+            }),
         icon: BitmapDescriptor.defaultMarker));
 
     _markers.add(Marker(
         markerId: MarkerId('This and That'),
         position: LatLng(45.178114, -93.873020),
-        infoWindow: InfoWindow(title: 'This and That', snippet: 'Decor'),
+        infoWindow: InfoWindow(
+            title: 'This and That',
+            snippet: 'Decor',
+            onTap: () {
+              Navigator.push(_myBuildContext,
+                  MaterialPageRoute(builder: (context) => SecondRoute()));
+            }),
         icon: BitmapDescriptor.defaultMarker));
 
     // i have a custom leaned out map style. no distracting features, minimal.
@@ -265,6 +336,28 @@ class _HomeAppState extends State<HomeApp> {
     super.initState();
   }
 
+  void filterStoreMarkersToOnlyWhatsOpen() {
+    Set<Marker> markersToRemove = {};
+
+    for (Marker marker in _markers) {
+      // for now, I'll just turn off all but 3 markers.
+      // later I'll put info in the Marker itself with store opening date/times
+      if ((marker.markerId.value == 'She') ||
+          (marker.markerId.value == 'Now and Again') ||
+          (marker.markerId.value == 'A Wreath of Franklin')) {
+        print('keeping this one' + marker.markerId.value);
+      } else {
+        markersToRemove.add(marker);
+      }
+    }
+    setState(() {
+      //Marker markerToRemove = _markers.first;
+      markersToRemove.forEach((element) {
+        _markers.remove(element);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     _myBuildContext = context;
@@ -272,15 +365,30 @@ class _HomeAppState extends State<HomeApp> {
     //what the hell, why doesn't mylocationenabled/button work???
     // because emulator.  try a real device and it works???
     //http://flutterdevs.com/blog/google-maps-in-flutter/
-    return GoogleMap(
-      myLocationButtonEnabled: true,
-      myLocationEnabled: true,
-      onMapCreated: _onMapCreated,
-      markers: Set<Marker>.of(_markers),
-      initialCameraPosition: CameraPosition(
-        target: _center,
-        zoom: 10.0,
+    return Stack(children: <Widget>[
+      GoogleMap(
+        myLocationButtonEnabled: true,
+        myLocationEnabled: true,
+        onMapCreated: _onMapCreated,
+        markers: Set<Marker>.of(_markers),
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 10.0,
+        ),
       ),
-    );
+      Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Align(
+          alignment: Alignment.topLeft,
+          child: FloatingActionButton(
+            // the toggle for only showing stores open right NOW
+            onPressed: () => filterStoreMarkersToOnlyWhatsOpen(),
+            materialTapTargetSize: MaterialTapTargetSize.padded,
+            backgroundColor: Colors.green,
+            child: const Icon(Icons.schedule, size: 36.0),
+          ),
+        ),
+      ),
+    ]);
   }
 }
---
+++ lib/secondroute.dart
@@ -0,0 +1,54 @@
+// blank mostly widget to test navigation and the back button
+import 'package:flutter/material.dart';
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Store Details"),
+      ),
+      body: Center(
+        child: Container(
+          width: 400,
+          height: 600,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            color: Colors.grey,
+            elevation: 10,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const ListTile(
+                  leading: Icon(Icons.store, size: 70),
+                  title:
+                      Text('Store Name', style: TextStyle(color: Colors.white)),
+                  subtitle:
+                      Text('-decor-', style: TextStyle(color: Colors.white)),
+                ),
+                // ButtonTheme.bar(
+                //   child: ButtonBar(
+                //     children: <Widget>[
+                //       FlatButton(
+                //         child: const Text('Edit',
+                //             style: TextStyle(color: Colors.white)),
+                //         onPressed: () {},
+                //       ),
+                //       FlatButton(
+                //         child: const Text('Delete',
+                //             style: TextStyle(color: Colors.white)),
+                //         onPressed: () {},
+                //       ),
+                //     ],
+                //   ),
+                // ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
