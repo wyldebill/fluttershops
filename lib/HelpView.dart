65d9e2b4e8cdaf3f35f6bc72259e74fccde0323a
william austin <william@v3solutions.net>
2021-02-07 14:30:04 -0600

    add help page for graphics attribution

---
+++ lib/HelpView.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HelpView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        "Graphic icons from www.vecteezy.com/free-vector",
+        style: TextStyle(fontSize: 20, color: Colors.black),
+      ),
+    );
+  }
+}
