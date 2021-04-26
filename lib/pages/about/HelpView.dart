f9581f3c52584d6386f1f1aceba507e09f84c5fe
william austin <william@v3solutions.net>
2021-04-25 21:29:54 -0500

    Remove images on device.  Add url launcher

--- lib/pages/about/HelpView.dart
+++ lib/pages/about/HelpView.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HelpView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        "Graphic icons from www.vecteezy.com/free-vector",
-        style: TextStyle(fontSize: 20, color: Colors.black),
-      ),
+    return Column(
+      children: [
+       
+        Text(
+          "Graphic icons from www.vecteezy.com/free-vector",
+          style: TextStyle(fontSize: 20, color: Colors.black),
+        ),
+       
+        const Padding(
+          padding: EdgeInsets.all(16.0),
+         
+        ),
+        ElevatedButton(
+          onPressed: () => {_launchInBrowser('https://wyldebill.github.io/Buffalo-Retail-Group-Privacy-Policy')},
+          child: const Text('Privacy Policy'),
+        ),
+      ],
     );
+  }
+
+  Future<void> _launchInBrowser(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: false,
+        forceWebView: false,
+        headers: <String, String>{'my_header_key': 'my_header_value'},
+      );
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
