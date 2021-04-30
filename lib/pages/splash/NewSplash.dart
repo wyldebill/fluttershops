3150c654fcaa7b3b61b27ea917a8272b5754156d
william austin <william@v3solutions.net>
2021-04-30 07:58:37 -0500

    Add on-device logo during startup

--- lib/pages/splash/NewSplash.dart
+++ lib/pages/splash/NewSplash.dart
@@ -7,28 +7,33 @@ import 'package:splashscreen/splashscreen.dart';
 // it should load things as it displays the spinner
 class NewSplash extends StatefulWidget {
   @override
+  
   _NewSplashState createState() => new _NewSplashState();
 }
 
 class _NewSplashState extends State<NewSplash> {
+
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
        theme: ThemeData(
         primarySwatch: Colors.teal),
       home: SplashScreen(
-          seconds: 2,
+          seconds: 3,
           navigateAfterSeconds: MyApp(),
           title:  Text(
-            'Buffalo Retail Group Maps',
+            'Buffalo Retail Group Map',
             style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0, color: Colors.white),
           ),
-          //image:  Image.network('https://i.imgur.com/TyCSG9A.png'),
+          image: Image.asset('assets/images/BRGLogo.jpg'),
           backgroundColor: Colors.teal,
           styleTextUnderTheLoader: TextStyle(),
-          photoSize: 100.0,
+          photoSize: 150.0,
           loaderColor: Colors.white),
           
     );
   }
 }
+
+ 
